7479c12eb2d98e1ad0f07a01eb3874b64b614c76
Priscila Patricio <priscilapatricio2@gmail.com>
2021-02-07 11:16:57 -0300

    resolvendo erros no código

--- script.js
+++ script.js
@@ -32,14 +32,16 @@ function reverseNumberFormat(num){
 }
 
 var operator = document.getElementsByClassName("operator");
-for(var i =0;i<operator.lenght;i++){
+
+for(var i=0; i < operator.length; i++){
     operator[i].addEventListener('click',function(){
+        
         if(this.id=="clear"){
             printHistory("");
             printOutput("");
         }
-        else if(this.id=="backspace"){
-            var output=reverseNumberFormat(getOutput()).toLocaleString;
+        else if(this.id == "backspace"){
+            var output=reverseNumberFormat(getOutput()).toString();
             if(output){
                 output= output.substr(0,output.length-1);
                 printOutput(output);
@@ -48,15 +50,16 @@ for(var i =0;i<operator.lenght;i++){
         else{
             var output=getOutput();
             var history=getHistory();
-            if(output==""&&history!=""){
+
+            if(output == "" && history != ""){
                 if(isNaN(history[history.length-1])){
-                    history=history.substr(0,history-length-1);
+                    history=history.substr(0,history.length-1);
                 }
             }
             if(output!="" || history!=""){
                 output= output==""?output:reverseNumberFormat(output);
                 history=history+output;
-                if(this.id==""){
+                if(this.id=="="){
                     var result=eval(history);
                     printOutput(result);
                     printHistory("");
@@ -72,12 +75,12 @@ for(var i =0;i<operator.lenght;i++){
 }
 
 var number = document.getElementsByClassName("number");
-for(var i=0; i<number.length; i++){
-    number[i].addEventListener('click', function(){
+for(var i =0; i<number.length;i++){
+    number[i].addEventListener('click',function(){
         var output=reverseNumberFormat(getOutput());
         if(output!=NaN){
             output=output+this.id;
             printOutput(output);
         }
-    })
+    });
 }
